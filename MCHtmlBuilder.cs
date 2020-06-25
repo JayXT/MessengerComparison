2fa573b341b6d648e39867aca9774d54fdfa1698
Yura Mocherniuk <yurii.mocherniuk@protonmail.com>
2020-06-25 12:48:33 +0300

    Fixed favicon.ico.

--- MCHtmlBuilder.cs
+++ MCHtmlBuilder.cs
@@ -32,7 +32,7 @@ namespace MessengerComparison
                     <head>
                         <meta http-equiv=""content-type"" content=""text/html; charset=utf-8"" />
                         <title>MessengerComparison</title>
-                        <link rel=""shortcut icon"" href=""favicon.ico"" type=""image/x-icon"">
+                        <link rel=""shortcut icon"" href=""../favicon.ico"" type=""image/x-icon"">
                         <link href=""https://fonts.googleapis.com/css?family=Noto+Sans"" rel=""stylesheet"">
                         <link rel=""stylesheet"" type=""text/css"" href=""../css/styles.css"">
                     </head>
